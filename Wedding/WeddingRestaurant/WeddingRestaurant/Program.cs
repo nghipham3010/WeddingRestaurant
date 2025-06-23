88277600b8e4629d06ad00a08808d7e05db97277
unknown <Pham Thanh Nghi>
2025-06-23 22:54:54 +0700

    Loi kha nhieu can sua

---
+++ Wedding/WeddingRestaurant/WeddingRestaurant/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WeddingRestaurant.Models;
+using WeddingRestaurant.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>() // Thêm hỗ trợ vai trò
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
---
+++ Wedding/WeddingRestaurant/WeddingRestaurant/Repositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿
+using WeddingRestaurant.Models;
+
+namespace WeddingRestaurant.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        // Các Repository cụ thể cho từng Model trong dự án tiệc cưới
+        IRepository<MonAn> MonAns { get; }      // Repository cho các Món Ăn
+        IRepository<Sanh> Sanhs { get; }        // Repository cho các Sảnh
+        IRepository<DatBan> DatBans { get; }    // Repository cho các Đặt Bàn
+
+        // Phương thức để lưu tất cả các thay đổi vào cơ sở dữ liệu
+        Task CompleteAsync();
+    }
+}
